7c1aa8966d9a022993e20195b24cd447e1350b17
Abuzarr123 <120338858+Abuzarr123@users.noreply.github.com>
2026-04-21 14:37:46 +0000

    using redis now as the storage instead of a volume

--- dockerfiles/run.sh
+++ dockerfiles/run.sh
@@ -2,7 +2,7 @@
 
 newData=`curl -s https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson`
 echo $newData | jq '.features[]' | jq '[.properties.place, .geometry.coordinates[0,1], .properties.mag]' | jq '((.[2]|tostring) + "|" + (.[1]|tostring) + "|" + .[0] + "|" + (.[3]|tostring))' | jq -r . > hour.chi
-./cliapp -i hour.chi --dataset-name 'all_hour'
+./cliapp -r -i hour.chi --dataset-name 'all_hour'
 rm hour.chi
 echo "Dataset 'all_hour' created."
 
